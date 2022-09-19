81db56e02200a862711bcce5e482c4f730673a62
npicciano79 <npicciano79@gmail.com>
2022-09-19 19:26:29 -0400

    fib recursive.js

--- fibRecursive.js
+++ fibRecursive.js
@@ -3,6 +3,7 @@
  * 
  * 
  */
+/*
 const fib=(num,memo={})=>{
     if (num in memo) return memo[num]
     if (num<2) {
@@ -17,3 +18,21 @@ const fib=(num,memo={})=>{
 //main driver
 let num=10
 console.log(fib(num))
+*/
+
+
+const fibonacciSeq=(num)=>{
+    if (num<2){
+        return num
+    }else{
+        return fibonacciSeq(num-1)+fibonacciSeq(num-2)    
+    }
+}
+
+const arr=[]
+num=10
+for (let i=1;i<=num;i++){
+    arr.push(fibonacciSeq(i))    
+}
+console.log(arr)
+
